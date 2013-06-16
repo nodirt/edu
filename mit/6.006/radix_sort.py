e19461ed1d7f84a80049abff15aba2e592f7ff76
nodirt <turakulov@gmail.com>
2013-06-16 11:08:30 -0400

    radix_sort calculates base as 2**(log2(n))

--- mit/6.006/radix_sort.py
+++ mit/6.006/radix_sort.py
@@ -3,18 +3,18 @@ from counting_sort import counting_sort
 import test
 
 def radix_sort(nums):
-	d = int(math.log10(max(nums))) + 1
-
-	m = 1
-	base = len(nums)
-	for i in xrange(d):
-		counting_sort(nums, base, key=lambda x: (x // m) % base)
-		m *= base
+    d = int(math.log10(max(nums))) + 1
+    n = len(nums)
+    r = int(math.log(n, 2))
+    base = 1 << r
+    mask = base - 1
+    for i in xrange(d):
+        counting_sort(nums, base, key=lambda x: (x >> r * i) & mask)
 
 
 def main():
-	test.test_sort(radix_sort)
-	print('All tests passed')
+    test.test_sort(radix_sort)
+    print('All tests passed')
 
 if __name__ == '__main__':
-	main()
+    main()

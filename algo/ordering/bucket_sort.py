71317b5b7f47b54026587e76aaf61c9f2af9e2c0
nodirt <turakulov@gmail.com>
2013-06-16 17:39:05 -0400

    Bucket sort

---
+++ algo/ordering/bucket_sort.py
@@ -0,0 +1,44 @@
+import test
+
+def bucket_sort(nums, min=0, max=1):
+    """Bucket sort. Sorts integers belonging to [min, max)
+
+    Complexity:
+        Time: O(n^2)
+        Average time: O(n)
+        Space: O(n)
+
+    Description:
+        Wikipedia: https://en.wikipedia.org/wiki/Bucket_sort
+        Cormen [1]: section 8.4, page 200
+    """    
+    n = len(nums)
+    buckets = [[] for i in xrange(n)]
+    for x in nums:
+        bi = int((x - min) * n / (max - min))
+        buckets[bi].append(x)
+
+    for b in buckets:
+        for i in xrange(1, len(b)):
+            x = b[i]
+            j = i
+            while j > 0 and b[j - 1] > x:
+                b[j] = b[j - 1]
+                j -= 1
+            b[j] = x
+        assert(test.is_sorted(b))
+
+    i = 0
+    for b in buckets:
+        for x in b:
+            nums[i] = x
+            i += 1
+
+
+def main():
+    test.test_sort(lambda nums: bucket_sort(nums, 0, 1000))  
+
+
+if __name__ == '__main__':
+    main()
+    print('All tests passed')

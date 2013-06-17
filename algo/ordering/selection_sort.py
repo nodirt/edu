6eab332f23b3f71734eb11270282dc78c7bd6512
nodirt <turakulov@gmail.com>
2013-06-16 23:06:26 -0400

    Selection sort

---
+++ algo/ordering/selection_sort.py
@@ -0,0 +1,31 @@
+import test
+
+
+def selection_sort(nums):
+    """Selection sort. Naive implementation is not practical.
+
+    Complexity:
+        Time: O(n&2)
+        Space: O(1)
+
+    Description:
+        Wikipedia: http://en.wikipedia.org/wiki/Selection_sort
+        Sedgewick [2]: section 2.1, page 248
+    """
+    n = len(nums)
+    for i in xrange(n):
+        smallest = i
+        for j in xrange(i + 1, n):
+            if nums[j] < nums[smallest]:
+                smallest = j
+
+        nums[i], nums[smallest] = nums[smallest], nums[i]
+
+
+def main():
+    test.test_sort(selection_sort)
+    print('All tests passed')
+
+
+if __name__ == '__main__':
+    main()

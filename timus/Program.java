caa8629b33f3797ed6bb42fc5a04b63a14a91160
nodirt <turakulov@gmail.com>
2013-08-02 14:38:18 -0400

    java template

---
+++ timus/Program.java
@@ -0,0 +1,18 @@
+import java.io.PrintWriter;
+import java.util.*;
+
+public class Program {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        PrintWriter out = new PrintWriter(System.out);
+
+        new Program().solve(in, out);
+        
+        out.flush();
+    }
+
+
+    void solve(Scanner in, PrintWriter out) {
+
+    }
+}
